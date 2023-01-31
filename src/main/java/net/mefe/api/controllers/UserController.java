3889cbd804c59f12224482dcac6bdebb19ea9f35
Mustafa FIRAT <mfirat@newmind.ai>
2023-01-31 16:25:24 +0300

    added folder structures

--- src/main/java/net/mefe/api/controllers/UserController.java
+++ src/main/java/net/mefe/api/controllers/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class LdapController {
+public class UserController {
 
     @RequestMapping("/")
     public String home() {
