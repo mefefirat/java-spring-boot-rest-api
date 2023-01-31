e342dae364112c4ddceb906895c3b7a60a0fd6ec
Mustafa FIRAT <mfirat@newmind.ai>
2023-01-31 18:39:30 +0300

    added new empty files

--- src/main/java/net/mefe/api/controllers/UserController.java
+++ src/main/java/net/mefe/api/controllers/UserController.java
@@ -5,12 +5,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.ArrayList;
+
 @RestController
 public class UserController {
 
-    @RequestMapping("/")
+
+
+    @RequestMapping("/users")
     public String home() {
-        return "Hello world.";
+
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+
+
+
+        return cars.toString();
     }
 
 }
